2e120f74fb90217c824c7883f00eccd7cdea524b
Neha Makru <137904719+Neha-Makru@users.noreply.github.com>
2025-09-09 17:05:49 +0530

    Update check_armstrong.py

--- check_armstrong.py
+++ check_armstrong.py
@@ -1,12 +1,13 @@
 def check_arm(n):
     sum, ori= 0,n
+    size=len(str(n))
     while n>0: 
         rem= n%10 
-        sum+=rem**3
+        sum+=rem**size
         n//=10 
     if sum==ori: 
         return "Armstrong Number"
     else: 
         return "Not an Armstrong Number"
     
-print(check_arm(153))
+print(check_arm(1634))
